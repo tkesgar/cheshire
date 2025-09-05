828f9a6e1039345038526fbd4326969af1f02cbd
Ted Kesgar <t.kesgar@outlook.com>
2025-09-06 02:46:26 +0700

    Remove nuxt content and add wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/content']
+  modules: ['@nuxt/eslint'],
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+      wrangler: {
+        name: "cheshire"
+      },
+    },
+  }
 })
